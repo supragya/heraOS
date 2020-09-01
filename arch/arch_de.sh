a60728092cca0f1f7414ea35f2e3eb4c4f704a02
Supragya Raj <supragyaraj@gmail.com>
2020-09-01 21:00:54 +0530

    mate setup

--- arch/arch_de.sh
+++ arch/arch_de.sh
@@ -1,5 +1,5 @@
 get_mate() {
-    pacman -S mate mate-session-manager dolphin atril engrampa rofi mate-media blueman network-manager-applet mate-power-manager mate-tweak
+    pacman -S mate mate-session-manager dolphin atril engrampa rofi mate-media blueman network-manager-applet mate-power-manager
 }
 
 #
